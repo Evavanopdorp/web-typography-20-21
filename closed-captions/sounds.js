25c1407d6fc8e994447eb5434b96d16497ad04db
Evavanopdorp <Evavanopdorp@users.noreply.github.com>
2021-03-09 22:36:44 +0100

    feedback Vasilis

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,15 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
+	1.65,
+	3.15,
 	9,
+	10.5,
 	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	15.5,
+	24.6,
+	31.2,
+	32.2,
+	70,
+	76
 ];
